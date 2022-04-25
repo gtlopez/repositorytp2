2d1813cb6ee9ec8d29c72bd8c7ecc29b7c39729e
GTL <GTL@DESKTOP-0LQB8T5>
2022-04-25 00:11:49 -0300

    commit primero

---
+++ TP2_Grupo_38/src/main/java/fi/unju/edu/ar/tp2/Tp2Grupo38Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.tp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo38Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo38Application.class, args);
+	}
+
+}
---
+++ TP2_Grupo_38/src/test/java/fi/unju/edu/ar/tp2/Tp2Grupo38ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.tp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2Grupo38ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
