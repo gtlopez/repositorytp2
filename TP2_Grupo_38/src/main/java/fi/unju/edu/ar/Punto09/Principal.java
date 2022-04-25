11ae65bfb26c50055f566cd184313f5b766baf03
Rx <Rx@ASUSRX>
2022-04-25 00:58:14 -0300

    Commit Punto09

---
+++ TP2_Grupo_38/src/main/java/fi/unju/edu/ar/Punto09/Principal.java
@@ -0,0 +1,16 @@
+package fi.unju.edu.ar.Punto09;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		System.out.println("Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás ");
+		System.out.println("utilizando un bucle for: ");
+		for (int i = 320; i >= 160; i -= 20) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
